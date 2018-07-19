4559e7d18281946ca42e5eaf1399e876b2d7638f
anujp41 <anuj.pant41@gmail.com>
2018-07-18 21:01:14 -0400

    did 1.2

--- Ch1/1.2.js
+++ Ch1/1.2.js
@@ -1,2 +1,9 @@
 // Check permutations
 
+function checkPerm(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  const arr1 = str1.split('');
+  arr1.sort((prev, curr) => str2.indexOf(prev) - str2.indexOf(curr));
+  str1 = attr1.join('');
+  return str1 === str2;
+}
